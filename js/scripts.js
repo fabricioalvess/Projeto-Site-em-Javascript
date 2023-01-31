bbe2fef91e2fcc34011bdadbb471b654ccf46081
Fabrício Cintra <fabricioalvescintra@gmail.com>
2023-01-31 19:47:30 -0300

    Foi adicionado o evento onclick ao menu responsivo

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 
 //Menu Mobile
-
+document.getElementById("hamburguer-icon").onclick = function(){
+  document.getElementById("sliding-header-menu-outer").style.right = "0";
+}
+document.getElementById("sliding-header-menu-close-button").onclick = function(){
+  document.getElementById("sliding-header-menu-outer").style.right = "-320px";
+}
 
 // About us Tab
 
